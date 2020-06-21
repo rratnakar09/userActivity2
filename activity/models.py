e80fa8bfdc572fe812c9a5afa5386cadc4ed0d39
Raman Ratnakar <43859527+rratnakar09@users.noreply.github.com>
2020-06-22 00:11:03 +0530

    Update models.py

--- activity/models.py
+++ activity/models.py
@@ -1,8 +1,7 @@
 from django.db import models
 
 # Create your models here.
-
-
+# User model will have fields like user id, user first name, user last name and user timezone
 class User(models.Model):
     id = models.CharField(max_length=20, primary_key=True)
     first_name = models.CharField(max_length=50)
@@ -12,7 +11,7 @@ class User(models.Model):
     def __str__(self):
         return self.id
 
-
+# ActivityTrack model will have user start time and end time. 
 class ActivityTrack(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     start_time = models.CharField(max_length=50)
