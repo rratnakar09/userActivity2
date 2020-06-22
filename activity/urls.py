890ea6e42054a75f95402870872933e99d6b521c
Raman Ratnakar <43859527+rratnakar09@users.noreply.github.com>
2020-06-23 03:35:04 +0530

    Update urls.py

--- activity/urls.py
+++ activity/urls.py
@@ -1,7 +1,11 @@
-from django.contrib import admin
 from django.urls import path
 from activity import views
 
 urlpatterns = [
-    path('get/', views.UserActivityView.as_view()),
+    path('users/', views.UserList.as_view()),
+    path('users/<str:pk>', views.UserDetail.as_view()), 
+    path('activities/', views.ActivityList.as_view()),
+    path('activities/<str:pk>/<int:id>', views.ActivityDetail.as_view()),
+    path('usersactivities/', views.UserActivityView.as_view()),
+    
 ]
